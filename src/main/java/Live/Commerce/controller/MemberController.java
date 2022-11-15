0a7882a23d33950bdf8b499e34bb9e135267bcc8
chaosworld69 <chaosworld69@naver.com>
2022-11-15 11:28:03 +0900

    first push

--- src/main/java/Live/Commerce/controller/MemberController.java
+++ src/main/java/Live/Commerce/controller/MemberController.java
@@ -1,9 +1,15 @@
 package Live.Commerce.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
+
+    @GetMapping
+    public String test() {
+        return "ok";
+    }
 
 
 }
